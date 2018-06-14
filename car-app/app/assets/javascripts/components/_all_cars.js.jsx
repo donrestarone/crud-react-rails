eea8cb4d3bd856235f7bf629580eb3cd1435b9c6
donrestarone <shashikejayatunge@gmail.com>
2018-06-14 19:42:34 -0400

    make second component for list of cars

---
+++ car-app/app/assets/javascripts/components/_all_cars.js.jsx
@@ -0,0 +1,9 @@
+class AllCars extends React.Component {
+	render() {
+		return(
+			<div>
+				<h1>List of cars</h1>
+			</div>
+		)
+	}
+}
--- car-app/app/assets/javascripts/components/_main.js.jsx
+++ car-app/app/assets/javascripts/components/_main.js.jsx
@@ -1,7 +1,10 @@
+//main component for the heading
+
 const Main = (props) => {
 	return(
 		<div>
 			<h1>Check These Cars Out!</h1>
+			<AllCars />
 		</div> 
 	)
 }
