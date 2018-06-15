fcade4b5ab606c601c7f2925982801418509d168
donrestarone <shashikejayatunge@gmail.com>
2018-06-14 20:06:59 -0400

    all cars component working for main page with the props returned by the server

--- car-app/app/assets/javascripts/components/_all_cars.js.jsx
+++ car-app/app/assets/javascripts/components/_all_cars.js.jsx
@@ -1,8 +1,26 @@
 class AllCars extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			cars: []
+		}
+	}
+	componentDidMount() {
+		fetch('/api/v1/cars.json').then((response) => {return response.json()}).then((data) => {this.setState({cars: data}) });
+	}
 	render() {
+		var cars = this.state.cars.map((car) => {
+			return(
+				<div key={car.id}>
+					<h1>{car.name}</h1>
+					<p>{car.description}</p>
+				</div>
+			)
+		})
 		return(
 			<div>
-				<h1>List of cars</h1>
+				{cars}
 			</div>
 		)
 	}
