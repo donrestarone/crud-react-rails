3474313d8960328d1fb69578a8710204389555f5
donrestarone <shashikejayatunge@gmail.com>
2018-06-17 14:03:09 -0400

    refactor

--- car-app/app/controllers/api/v1/cars_controller.rb
+++ car-app/app/controllers/api/v1/cars_controller.rb
@@ -4,12 +4,12 @@ class Api::V1::CarsController < ApplicationController
 	end
 
 	def create
-		car = Car.new(car_params)
-		if car.save
-			call_giphy(car, car.picture)
-			render json: car
+		@car = Car.new(car_params)
+		@car = Car.call_giphy(@car, @car.picture)
+		if @car.save
+			render json: @car
 		else 
-			render json: car.errors, status: :bad_request
+			render json: @car.errors, status: :bad_request
 		end
 	end
 
@@ -21,7 +21,8 @@ class Api::V1::CarsController < ApplicationController
 
 		car = Car.find(params[:id])
 		if car.update(car_params)
-			call_giphy(car, car.name)
+			car = Car.call_giphy(car, car.name)
+			car.save!
 			render json: car
 		else 
 			render json: {
@@ -34,24 +35,5 @@ class Api::V1::CarsController < ApplicationController
 
 	def car_params
 		params.require(:car).permit(:id, :name, :description)
-	end
-
-	def find_gif(giphy_response)
-		gif_array = []
-		while gif_array.count < 10
-			giphy_response.each do |array_index|
-				gif_array.push array_index["images"]["fixed_height"]["url"]
-
-			end
-		end
-		return gif_array
-	end
-
-	def call_giphy(car, search)
-		key = ENV['GIPHY_KEY']
-		giphy_response = JSON.parse((HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{key}&q=#{car.name}")).body)
-		gif = find_gif(giphy_response["data"])
-		car.picture = gif.sample
-		car.save
 	end
 end
--- car-app/app/models/car.rb
+++ car-app/app/models/car.rb
@@ -1,4 +1,23 @@
 class Car < ApplicationRecord
 	validates :name, presence: true, allow_blank: false
 	#validates :description, presence: true, allow_blank: false
+
+	def self.call_giphy(car, picture)
+		key = ENV['GIPHY_KEY']
+		giphy_response = JSON.parse((HTTParty.get("http://api.giphy.com/v1/gifs/search?api_key=#{key}&q=#{car.name}")).body)
+		gif = Car.find_gif(giphy_response["data"])
+		car.picture = gif.sample
+		return car
+	end
+
+	def self.find_gif(giphy_response)
+		gif_array = []
+		while gif_array.count < 10
+			giphy_response.each do |array_index|
+				gif_array.push array_index["images"]["fixed_height"]["url"]
+
+			end
+		end
+		return gif_array
+	end
 end
